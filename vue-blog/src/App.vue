e03badb51ec2160fa2f8d452779c70be6e5592d6
louisxiv <502990436@qq.com>
2016-12-02 18:09:26 +0800

    加入头像

--- vue-blog/src/App.vue
+++ vue-blog/src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <img src="../static/images/lg.jpg" alt="ME" width="100" id="ME">
     <div class="header">
   <router-link to="/home">Home</router-link>
   <router-link to="/aboutus">Abouts</router-link>
   <router-link to="/product">Product</router-link>
   <router-link to="/news">News</router-link>
   <router-link to="/service">Service</router-link>
-  <router-link to="/foo">Go to Foo</router-link>
-  <router-link to="/bar">Go to Bar</router-link>
-  <router-view></router-view>
+  <!-- <router-link to="/foo">Go to Foo</router-link>
+  <router-link to="/bar">Go to Bar</router-link> -->
+  <router-view keep-alive transition="fade" transition-mode='out-in'></router-view>
 </div>
   </div>
 </template>
@@ -34,7 +34,9 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
-
+#ME{
+    border-radius: 50%;
+}
 h1, h2 {
   font-weight: normal;
 }
@@ -51,5 +53,11 @@ li {
 
 a {
   color: #42b983;
+}
+.fade-transition {
+ transition: opacity .3s ease;
+}
+.fade-enter, .fade-leave {
+ opacity: 0;
 }
 </style>
