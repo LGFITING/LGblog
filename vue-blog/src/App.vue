564b8e07ea1579d9c2ffd358ea460db3d7e167d3
louisxiv <502990436@qq.com>
2016-12-04 00:49:30 +0800

    博客

--- vue-blog/src/App.vue
+++ vue-blog/src/App.vue
@@ -2,22 +2,37 @@
   <div id="app">
 <header>
    <div id="header">
-
+  <div class="defind">
+    <h3>肆的博客</h3>
+    <p>github地址：
+      <a href="https://github.com/LGFITING/">https://github.com/LGFITING</a>
+    </p>
+    <ul>
+      <li><router-link to="/home">首页</router-link></li>
+      <li><a href="#">|</a></li>
+      <li>  <router-link to="/product">博文目录</router-link></li>
+      <li><a href="#">|</a></li>
+      <li>  <router-link to="/news">图文心情</router-link></li>
+      <li><a href="javascript:return false;">|</a></li>
+      <li><router-link to="/aboutus">关于我</router-link></li>
+    </ul>
+  </div>
    </div>
 </header>
 <content>
   <div id="content">
   <xivprofile></xivprofile>
+    <router-view keep-alive transition="fade" transition-mode='out-in'></router-view>
   </div>
 </content>
-  <router-link to="/home">Home</router-link>
+  <!-- <router-link to="/home">Home</router-link>
   <router-link to="/aboutus">Abouts</router-link>
   <router-link to="/product">Product</router-link>
   <router-link to="/news">News</router-link>
-  <router-link to="/service">Service</router-link>
+  <router-link to="/service">Service</router-link> -->
   <!-- <router-link to="/foo">Go to Foo</router-link>
   <router-link to="/bar">Go to Bar</router-link> -->
-  <router-view keep-alive transition="fade" transition-mode='out-in'></router-view>
+  <!-- <router-view keep-alive transition="fade" transition-mode='out-in'></router-view> -->
   </div>
 </template>
 
@@ -43,6 +58,10 @@ export default {
 }
 body{
   background: #e7eef4;
+  font-size: "微软雅黑","黑体";
+}
+a{
+  text-decoration: none;
 }
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
@@ -55,17 +74,42 @@ body{
 }
 #header{
   width: 100%;
-  height: 200px;
+  height: 40%;
   position: absolute;
   top: 0px;
-  background: url('../static/images/sinablogb.jpg') -100px 0px;
+  background: url('../static/images/sinablogb.jpg') -100px 0px no-repeat;
+  background-position: center top;
+}
+.defind{
+  position: relative;;
+  left: 300px;
+  top: 130px;
+}
+.defind p{
+  font-size: 12px;
+  font-weight: bold;
+  padding: 6px 0;
+}
+.defind ul li{
+  color: #42b983;
+  margin-left: 0px;
+}
+.defind ul li a{
+    font-size: 14px;
+  transition:opacity .5s,font-size .5s;
+}
+.defind ul li a:hover{
+  opacity: .5;
+  font-size: 18px;
+
 }
 #content{
   border: 1px solid red;
-  width: 1150px;
-  margin: 240px 100px 0px 100px;
+  width:960px;
   height: 500px;
   padding: 10px;
+  margin: 300px auto;
+
 }
 h1, h2 {
   font-weight: normal;
